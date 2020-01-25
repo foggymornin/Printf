2678b950ecf89bbaa67ccdfd5d788e9747e1df7c
Marouane Fajat <mafajat@e1r10p11.1337.ma>
2020-01-25 22:39:43 +0100

    helo

---
+++ ft_printf/ft_pint.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pint.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafajat <mafajat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/22 17:04:25 by mafajat           #+#    #+#             */
+/*   Updated: 2020/01/24 22:49:59 by mafajat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int     ft_pint(int n)
+{
+    int w;
+    int zero;
+    char *s;
+    
+    if (n == 0 && f.p == 0)
+        return (ft_handle_zero_i());
+    s = ft_itoa(n);
+    w = 0;
+    f.type = 'i';
+    if (f.p <= ft_strlen(s))
+        w = f.width - ft_strlen(s);
+    else
+        w  = f.width - f.p;
+    zero = f.p - ft_strlen(s);
+    if (*s == '-' && f.z == '0' && f.p == -1)
+        ft_putchar(*s++);
+    if (f.minus == '-')
+    {
+        if (*s == '-' && zero++ > 0)
+            ft_putchar(*s++);
+            while(zero-- > 0)
+            ft_putchar('0');
+        ft_putstr(s);
+    }
+    while (!(*s == '-' && zero > 0 && w == 1) && w-- > 0)
+    {
+        if (f.z == '0' && f.p < 0 && f.minus != '-')
+            ft_putchar('0');
+        else 
+            ft_putchar(' ');
+    }
+    if (f.minus != '-')
+    {
+        if (*s == '-' && zero++ > 0)
+            ft_putchar(*s++);
+        while(zero-- > 0)
+            ft_putchar('0');
+        ft_putstr(s);
+    } 
+   // free(s);
+    return (0);
+}
